cfa5717f3a90f8f89a8a383e65cc141eccddd95a
vtdt <127406000+vtdt@users.noreply.github.com>
2024-02-28 15:21:13 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
 <?php
+include "tools.php";
 
+$mydb = new conquer("mysql:host=localhost;port=3306;dbname=blog_vahramejevs;user=root;password=;charset=utf8mb4;");
 
+// $mydb->send_query("INSERT INTO posts(title) VALUES ('My fourth blog from PHP PDO');");
+// $mydb->send_query("SELECT * FROM posts;");
+// $mydb->dump();
+$mydb->dump_table("posts");
 
+$mexico = "mysql:host=localhost;port=3306;dbname=blog_vahramejevs;user=root;password=;charset=utf8mb4;";
+$dbcon = new PDO($mexico);
+$quer = $dbcon->prepare("SELECT * FROM posts");
+$quer->execute();
+$quer->fetchAll();
+
+// dd($quer->fetchAll());
 ?>
